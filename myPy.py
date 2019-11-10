a6596866dafee36021c018c7e32441a2408fc348
Sanaa-hub <56192643+Sanaa-hub@users.noreply.github.com>
2019-11-10 15:40:26 +0000

    Update myPy.py

--- myPy.py
+++ myPy.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "<h1 style='color:blue'>testing live 3333333333333</h1>"
+    return "<h1 style='color:blue'>testing live 4444444444444</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
